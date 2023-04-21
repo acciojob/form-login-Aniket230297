100fe6b00643adc5ebe50a1afecc8b7dc133743c
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-04-21 20:25:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	var fame=document.getElementById("fname").value;
+    var lame=document.getElementById("lname").value;
+	
+	alert(fame+" "+lame);
 
 }
+//alert(fame)
